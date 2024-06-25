bb42f1607712ce9b9affbce1e1907dc3b4479833
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-06-25 20:13:25 +0300

    make build

---
+++ index.d78d19e5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=e.querySelector("thead"),o=e.querySelector("tbody"),t=r.querySelector("tr"),c=e.querySelector("tfoot"),l=c.querySelector("tr"),n=c.querySelector("tr").children,u=n[1].cloneNode(!0);l.insertBefore(u,n[4]);var y=r.querySelector("tr").children,d=y[1].cloneNode(!0);t.insertBefore(d,y[4]),o.querySelectorAll("tr").forEach(function(e){var r=e.children,o=r[1].cloneNode(!0);e.insertBefore(o,r[4])});
+//# sourceMappingURL=index.d78d19e5.js.map
